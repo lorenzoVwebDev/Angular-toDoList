73588976644283c24638a71f3ebb672c465e7899
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-05-11 17:51:32 +0200

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule, MatCardModule],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
